c47b7d3ad8a52cff9ecfcdd39ccfb33fb5197322
chandan <chandanramawat2004@gmail.com>
2026-08-22 22:53:59 +0530

    redesign the backend feature

--- backend/main.py
+++ backend/main.py
@@ -10,14 +10,19 @@ from fastapi.responses import StreamingResponse
 app = FastAPI(title="AI travel assistant")
 from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-       CORSMiddleware,
-       allow_origins=["https://tripsaarthi-ml089va0p-krishna-0305.vercel.app"],
-       allow_credentials=True,
-       allow_methods=["*"],
-       allow_headers=["*"],
-   )
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(title="AI travel assistant")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://tripsaarthi-28vm294l2-krishna-0305.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # MODELS
 class ChatRequest(BaseModel):
