d733566a7e997c672981a56be406dc4d544c0f45
chandan <chandanramawat2004@gmail.com>
2026-08-22 22:33:28 +0530

    fix backend issue

--- backend/main.py
+++ backend/main.py
@@ -8,16 +8,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 app = FastAPI(title="AI travel assistant")
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "https://tripsaarthiai.vercel.app",
-    ],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+       CORSMiddleware,
+       allow_origins=["https://tripsaarthi-ml089va0p-krishna-0305.vercel.app"],
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 
 
 # MODELS
